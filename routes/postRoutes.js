c116c80e9c6d91bd988dbcc8f81457aa52163aec
bibav777 <you@example.com>
2020-02-12 02:12:25 -0800

    added different responses with url

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -6,8 +6,8 @@ const postModel= require('../model/Posts.js');
 router.post("/addpost",(req,res) => {
 
     data = {
-        "postdescription"    : req.body.postdescription,
-        "postimage"   : req.body.postimage,
+        "postdescription": req.body.postdescription,
+        "postimage":req.body.postimage,
         "posteddate":req.body.posteddate
 
     }
@@ -16,7 +16,16 @@ router.post("/addpost",(req,res) => {
   addPost.save().then(result=>{
      res.status(200).json({
        "message":"Post added Sucessfully",
-       "result":result
+       posts:{
+         id:result._id,
+         postdescription:result.postdescription,
+         postimage:result.postimage,
+         posteddate:result.posteddate
+       },
+       type:{
+         request:"POST",
+         url:"http://localhost:8000/post"
+       }
      })
    }).catch(err=>{
 
@@ -26,14 +35,57 @@ router.post("/addpost",(req,res) => {
 });
 
 
-router.get('/posts',(req,res,next)=>{
-postModel.find().exec().
-then(result=>{
-res.status(200).json({"Message":"All posts","Posts":result})
+router.get('/',(req,res,next)=>{
+postModel.find().select('_id postdescription postimage posteddate ').exec().
+then(results=>{
+  const response= {
+        count: results.lenght,
+        posts: results.map(result=>{
+            return{
+              _id: result._id,
+              postdescription:result.postdescription,
+              postimage:result.postimage,
+              posteddate:result.posteddate,
+
+              type:{
+                 request: "GET",
+                 url:"http://localhost:8000/post/"+ result._id
+               }
+            }
+
+
+        })
+
+
+  }
+res.status(200).json({"Message":"All posts","Posts":response})
 
 }).catch(error=>{
   res.status(500).json({"Error":error})
 })
+});
+
+
+router.get('/:postid',(req,res,next)=>{
+const id= req.params.postid
+postModel.findById(id).select('_id postdescription postimage posteddate').exec().
+then(results=>{
+res.status(200).json({
+"Post":results,
+type:{
+  request:"GET",
+  url:"http://localhost:8000/post/" + results._id
+}
+
+})
+
+}).catch(err=>{
+  res.status(500).json({
+    "message":"Error finding Post"
+  })
+})
+
+
 
 
 });
