f096826f368045636e5023c3778cd5ba23967343
bibav777 <you@example.com>
2020-02-15 23:50:55 -0800

    Uploading multiple images functionality created

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,131 +1,145 @@
-const express= require('express');
-const router= express.Router();
-const multer= require('multer');
-const postModel= require('../model/Posts.js');
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const postModel = require('../model/Posts.js');
 
 //for stroing image destination and filename
-const storage= multer.diskStorage({
-destination:(req,file,cb)=>{
- cb(null, './uploads/');
- },
-filename:(req,file,cb)=>{
- cb(null,   Date.now()+file.originalname );
- }
- });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + file.originalname);
+    }
+});
 
- //filefilter for only selected type of image is inserted to database
-const fileFilter= (req,file,cb)=>{
-if(file.mimetype ==='image/png' || file.mimetype==='image/jpeg' || file.mimetype==='image/jpg' ){
-cb(null,true)
-}else{
-cb(null,false)
-  }
+//filefilter for only selected type of image is inserted to database
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg') {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
 };
-const upload= multer({storage:storage,fileFilter:fileFilter});
+const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 
 //route to add a new post
-router.post("/addpost",upload.single('postimage'),(req,res) => {
-        console.log(req.file);
+router.post("/addpost", upload.array('postimage', 10), (req, res) => {
     data = {
         "postdescription": req.body.postdescription,
-        "postimage":req.file.path,
-        "posteddate":req.body.posteddate
-      }
-const addPost= new postModel(data);
-  addPost.save().then(result=>{
-     res.status(200).json({
-       "message":"Post added Sucessfully",
-       posts:{
-         id:result._id,
-         postdescription:result.postdescription,
-         postimage:result.postimage,
-         posteddate:result.posteddate
-       },
-       type:{
-         request:"GET",
-         url:"http://localhost:8000/post/"+result._id
-       }
-     })
-   }).catch(err=>{
+        "postimage": req.files.map(file => {
+            const imagePath = file.path;
+            return imagePath
+        }),
+        "posteddate": req.body.posteddate
+    }
 
-     res.status(500).json({"message":"Error creating Post"})
-   })
+    console.log(data)
+    const addPost = new postModel(data);
+    addPost.save().then(result => {
+        res.status(200).json({
+            "message": "Post added Sucessfully",
+            posts: {
+                id: result._id,
+                postdescription: result.postdescription,
+                postimage: result.postimage,
+                posteddate: result.posteddate
+            },
+            type: {
+                request: "GET",
+                url: "http://localhost:8000/post/" + result._id
+            }
+        })
+    }).catch(err => {
+
+        res.status(500).json({ "message": "Error creating Post" })
+    })
 
 });
 
 //route to fetch all posts from database
-router.get('/',(req,res,next)=>{
-postModel.find().select('_id postdescription postimage posteddate ').exec().
-then(results=>{
-  const response= {
-        count: results.length,
-        posts: results.map(result=>{
-            return {
-              _id: result._id,
-              postdescription:result.postdescription,
-              postimage:result.postimage,
-              posteddate:result.posteddate,
-              type:{
-                 request: "GET",
-                 url:"http://localhost:8000/post/"+ result._id
-               }
-            }})
-          }
-res.status(200).json({"Message":"All posts","Posts":response})
-}).catch(error=>{
-  res.status(500).json({"Error":error})
-})
+router.get('/', (req, res, next) => {
+    postModel.find().select('_id postdescription postimage posteddate ').exec().
+    then(results => {
+        const response = {
+            count: results.length,
+            posts: results.map(result => {
+                return {
+                    _id: result._id,
+                    postdescription: result.postdescription,
+                    postimage: result.postimage,
+                    posteddate: result.posteddate,
+                    type: {
+                        request: "GET",
+                        url: "http://localhost:8000/post/" + result._id
+                    }
+                }
+            })
+        }
+        res.status(200).json({ "Message": "All posts", "Posts": response })
+    }).catch(error => {
+        res.status(500).json({ "Error": error })
+    })
 });
 
 //route to fetch posts from id
-router.get('/:postid',(req,res,next)=>{
-const id= req.params.postid
-postModel.findById(id).select('_id postdescription postimage posteddate').exec().
-then(results=>{
-res.status(200).json({
-"Post":results,
-type:{
-  request:"GET",
-  url:"http://localhost:8000/post/" + results._id
-}
-})
-}).catch(err=>{
-  res.status(500).json({
-    "message":"Error finding Post"
-  })
-})
+router.get('/:postid', (req, res, next) => {
+    const id = req.params.postid
+    postModel.findById(id).select('_id postdescription postimage posteddate').exec().
+    then(results => {
+        res.status(200).json({
+            "Post": results,
+            type: {
+                request: "GET",
+                url: "http://localhost:8000/post/" + results._id
+            }
+        })
+    }).catch(err => {
+        res.status(500).json({
+            "message": "Error finding Post"
+        })
+    })
 });
 
 //route for update Posts
-router.patch('/:postid',upload.single('postimage'),(req,res,next)=>{
-const id=req.params.postid;
-postModel.updateOne({_id:id,},{$set:{postdescription:req.body.postdescription,
- postimage:req.file.path, posteddate:req.body.posteddate}})
-.exec().then(results=>{
-res.status(200).json({
-  "Updated Product":results
-})
-}).catch(err=>{
- res.json({
-   "Message": "Error updating post"
- })
-})
+router.patch('/:postid', upload.array('postimage', 10), (req, res, next) => {
+    const id = req.params.postid;
+    postModel.updateOne({ _id: id, }, {
+            $set: {
+                postdescription: req.body.postdescription,
+                postimage: req.files.map(file => {
+                    const imagePath = file.path;
+                    return imagePath
+
+                }),
+                posteddate: req.body.posteddate
+            }
+        })
+        .exec().then(results => {
+            res.status(200).json({
+                "Updated Product": results
+            })
+        }).catch(err => {
+            res.json({
+                "Message": "Error updating post"
+            })
+        })
 });
 
 
 //route for deleting posts for database
-router.delete('/:postid',(req,res,next)=>{
-const id= req.params.postid;
-postModel.remove({_id:id}).exec().
-then(result=>{
-  res.status(200).json({
-    message:'Post deleted successfully'
-  })
-}).
-catch(err=>{
-  res.status(500).json({"Error":error})
-})
+router.delete('/:postid', (req, res, next) => {
+    const id = req.params.postid;
+    postModel.remove({ _id: id }).exec().
+    then(result => {
+        res.status(200).json({
+            message: 'Post deleted successfully'
+        })
+    }).
+    catch(err => {
+        res.status(500).json({ "Error": error })
+    })
 })
 
-module.exports=router;
+module.exports = router;
