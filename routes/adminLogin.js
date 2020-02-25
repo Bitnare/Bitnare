55ca4e33565e84868a4b8f59d1ffa601c6fe7059
poonamlama <poonamlama253@gmail.com>
2020-02-25 13:46:12 +0545

    added search user by age

--- routes/adminLogin.js
+++ routes/adminLogin.js
@@ -4,6 +4,46 @@ const admin = require("../model/admin");
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+// admin register
+// router.post("/addadmin",(req,res) => {
+    
+//         var password = req.body.password;
+    
+//         bcrypt.genSalt(saltRounds, function (err, salt) {
+//             if (err) {
+//                 throw err
+//             } else {
+//                 bcrypt.hash(password, salt, function(err, hashedPassword){
+//                     if (err) {
+//                         throw err
+//                     } else {
+    
+//                         data = {
+//                             "username"      : req.body.username,
+//                             "password"      : hashedPassword
+//                         }
+    
+//                             var addadmin= new admin(data);
+//                             addadmin.save().then(function(){
+//                             res.send({
+//                                 message:"Sucessful "
+//                             });
+    
+                            
+//                         }).catch(err => {
+//                             res.status(500).send(
+//                                 err.errors
+//                             );
+//                         });
+    
+//                     }
+    
+//                 })
+//             }
+//         })
+//     });
+
+
 
 //admin login
 router.post('/adminlogin', async function(req, res){
@@ -21,9 +61,11 @@ router.post('/adminlogin', async function(req, res){
             const admins = await admin.checkCrediantialsDb(req.body.username, req.body.password);
             if (admins) {
                 var id = admins._id;
+                const username = admins.username;
 
                 res.send({
                     id, 
+                    username,
                     message: "Login sucess"
                 });
 
--- routes/userSearch.js
+++ routes/userSearch.js
@@ -2,57 +2,104 @@ const express = require('express');
 const router = express.Router();
 const user = require("../model/register");
 
-router.post('/searchUser', function (req, res){
+// router.post('/searchUser', function (req, res){
 
-    var job = req.body.job_title;
-    var skills = req.body.skills;
-    var gender = req.body.gender;
+//     var job = req.body.job_title;
+//     var skills = req.body.skills;
+//     var gender = req.body.gender;
+//     var min_age = req.body.min_age;
+//     var max_age = req.body.max_age;
+
+//     // console.log(min_age, max_age);
+//     var current = new Date();
+//     // var min_age = new min_age();
+//     // console.log(min_age);
+
+//     var min = current.getFullYear()-min_age;
+//     var max = current.getFullYear()-max_age;
+  
+//     var min_range = new Date(min+"-"+current.getUTCMonth()+"-"+current.getUTCDay());
+//     var max_range = new Date(max+"-"+current.getUTCMonth()+"-"+current.getUTCDay());
+   
+
+//     console.log(min_range);
+//     console.log(max_range);
+//     user.find({ 
+//         $or:[
+//             {'job_title' : new RegExp (job, 'i')},
+//             {'skills' : new RegExp (skills, 'i')},
+//             {'gender' : new RegExp (gender, 'i')}
+//         ],
+
+//         $and:[
+//             {'dob': {$gt:min_range}}
+//         ]
+//         }).then (function (listing) {
+//         console.log(listing);
+//         // if(listing){
+//         //     var dob =listing[0].dob;
+//         //         var current = new Date();
+//         //         var dob = new Date(dob);
+
+//         //     var datediff=current.getFullYear()-dob.getFullYear();
+//         //     var age = datediff;
+//         // }
+//         res.send(listing);
+//         // console.log(age);
+
+//     }).catch (function (e){
+//         res.send(e) 
+//     });
+
+// });
+
+router.post('/searchUser', function (req, res) {
+
     var min_age = req.body.min_age;
     var max_age = req.body.max_age;
 
-    // console.log(min_age, max_age);
     var current = new Date();
     // var min_age = new min_age();
-    // console.log(min_age);
+    console.log(min_age + "-" + max_age);
 
-    var min = current.getFullYear()-min_age;
-    var max = current.getFullYear()-max_age;
-  
-    var min_range = new Date(min+"-"+current.getUTCMonth()+"-"+current.getUTCDay());
-    var max_range = new Date(max+"-"+current.getUTCMonth()+"-"+current.getUTCDay());
-   
+    var min = current.getFullYear() - min_age;
+    var max = current.getFullYear() - max_age;
+
+    var min_range = new Date(min + "-" + current.getUTCMonth() + "-" + current.getUTCDay());
+    var max_range = new Date(max + "-" + current.getUTCMonth() + "-" + current.getUTCDay());
 
     console.log(min_range);
     console.log(max_range);
-    user.find({ 
-        $or:[
-            {'job_title' : new RegExp (job, 'i')},
-            {'skills' : new RegExp (skills, 'i')},
-            {'gender' : new RegExp (gender, 'i')}
-        ],
 
-        $and:[
-            {'dob': {$gt:min_range}}
-        ]
-        }).then (function (listing) {
+    user.find({
+         $or : [{
+        "location" :    { $near :
+            {
+              $geometry : {
+                 type : "Point" ,
+                 coordinates : [req.body.longitude,req.body.latitude] },
+              $maxDistance : 1000*10 // gets users within 10 km maximum distance
+            }
+         },
+
+        'dob': {
+            $gte: max_range,
+            $lte: min_range
+        }
+    }]
+
+    
+    }).then(function (listing) {
         console.log(listing);
-        // if(listing){
-        //     var dob =listing[0].dob;
-        //         var current = new Date();
-        //         var dob = new Date(dob);
 
-        //     var datediff=current.getFullYear()-dob.getFullYear();
-        //     var age = datediff;
-        // }
         res.send(listing);
-        // console.log(age);
 
-    }).catch (function (e){
-        res.send(e) 
+
+    }).catch(function (e) {
+        res.send(e)
     });
 
 });
-
 
 module.exports = router;
 
